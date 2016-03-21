5d73f61c8b89e81688ed9edcb8aeb6adbd61226a
sayhien <sayhien@gmail.com>
2016-03-21 16:17:26 +0800

    Added files via upload

---
+++ RESOURCES/JS/bishanweb.js
@@ -0,0 +1,211 @@
+/* audio */
+
+/* title */
+
+function play1() {
+    var audio = document.getElementById("audio1");
+    audio.play();
+}
+
+/* bishan */
+function play2() {
+    var audio = document.getElementById("audio2");
+    audio.play();
+}
+
+/* J8 */
+function play3() {
+    var audio = document.getElementById('audio3');
+    audio.play();
+}
+
+/* gym */
+function play4() {
+    var audio = document.getElementById('audio4');
+    audio.play();
+}
+
+/* library */
+function play5() {
+    var audio = document.getElementById('audio5');
+    audio.play();
+}
+
+/* over */
+function play14() {
+    var audio = document.getElementById('audio14');
+    audio.play();
+}
+
+/* doge */
+function playdoge1() {
+    var audio = document.getElementById('audio-doge1');
+    audio.play();
+}
+
+function playdoge2() {
+    var audio = document.getElementById('audio-doge2');
+    audio.play();
+}
+
+function playdoge3() {
+    var audio = document.getElementById('audio-doge3');
+    audio.play();
+}
+
+function playdoge4() {
+    var audio = document.getElementById('audio-doge4');
+    audio.play();
+}
+
+function playdoge4() {
+    var audio = document.getElementById('audio-doge4');
+    audio.play();
+}
+
+
+function playdoge5() {
+    var audio = document.getElementById('audio-doge5');
+    audio.play();
+}
+
+function playdoge6() {
+    var audio = document.getElementById('audio-doge6');
+    audio.play();
+}
+
+
+function playdoge7() {
+    var audio = document.getElementById('audio-doge7');
+    audio.play();
+}
+
+
+function playdoge8() {
+    var audio = document.getElementById('audio-doge8');
+    audio.play();
+}
+
+function playdoge9() {
+    var audio = document.getElementById('audio-doge9');
+    audio.play();
+}
+
+
+function playdoge10() {
+    var audio = document.getElementById('audio-doge10');
+    audio.play();
+}
+
+
+function playdoge11() {
+    var audio = document.getElementById('audio-doge11');
+    audio.play();
+}
+
+function playdoge12() {
+    var audio = document.getElementById('audio-doge12');
+    audio.play();
+}
+
+function playdoge13() {
+    var audio = document.getElementById('audio-doge13');
+    audio.play();
+}
+
+function playdoge14() {
+    var audio = document.getElementById('audio-doge14');
+    audio.play();
+}
+
+
+
+
+jQuery(function ($) {
+    $('#curltext').circleType({
+        radius: 144
+    });
+})
+
+jQuery(function ($) {
+    $('#curltext-j8').circleType({
+        radius: 144
+    });
+})
+
+jQuery(function ($) {
+    $('#curltext-gyms').circleType({
+        radius: 144
+    });
+})
+
+jQuery(function ($) {
+    $('#curltext-library').circleType({
+        radius: 144
+    });
+})
+
+jQuery(function ($) {
+    $('#curltext-park').circleType({
+        radius: 144
+    });
+})
+
+jQuery(function ($) {
+    $('#curltext-restaurants').circleType({
+        radius: 144
+    });
+})
+
+jQuery(function ($) {
+    $('#curltext-dog').circleType({
+        radius: 144
+    });
+})
+
+jQuery(function ($) {
+    $('#curltext-activities').circleType({
+        radius: 144
+    });
+})
+
+jQuery(function ($) {
+    $('#curltext-heritage').circleType({
+        radius: 144
+    });
+})
+
+jQuery(function ($) {
+    $('#curltext-catchment').circleType({
+        radius: 155
+    });
+})
+
+jQuery(function ($) {
+    $('#curltext-uppthomson').circleType({
+        radius: 155
+    });
+})
+
+jQuery(function ($) {
+        $('#curltext-temple').circleType({
+            radius: 174
+        });
+    })
+    /*
+    var $sound=$('.sound-eff').hide();
+    $('.cat-begin').on('click', function(){
+        $sound.fadeToggle();
+        setTimeout(function() {$('.sound-eff').fadeOut();
+                    },
+                2000);
+    })
+
+    /*
+    var $sound=$('.sound-eff-2').hide();
+    $('.cat-begin-2').on('click', function(){
+        $sound.fadeToggle();
+        setTimeout(function() {$('.sound-eff-2').fadeOut();
+                    },
+                2000);
+    })*/
